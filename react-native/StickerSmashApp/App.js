b1df7dd805de4e97b7798b7b5c15b585abee1136
Li_Tian <Li_Tian@singaporeair.com.sg>
2024-03-15 21:15:31 +0800

    rn-stickersmashapp: add ImagePicker, installed  expo-image-picker
    note:
    use import * as ImagePicker from 'expo-image-picker'
    don't use import ImagePicker from 'expo-image-picker'

--- react-native/StickerSmashApp/App.js
+++ react-native/StickerSmashApp/App.js
@@ -1,3 +1,4 @@
+import * as ImagePicker from 'expo-image-picker';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Button from './components/Button';
@@ -6,33 +7,46 @@ import ImageViewer from './components/ImageViewer';
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer} >
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
-      </View>
-      <View style={styles.footerContainer} >
-        <Button theme='primary' label="Choose a photo" />
-        <Button label="Use this photo" />
-      </View>
-      <StatusBar style="auto" />
-    </View >
-  );
+    const pickImageAsync = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            allowEditing: true,
+            quality: 1,
+        })
+
+        if (!result.cancelled) {
+            console.log(result)
+        } else {
+            alert('you did not select any image')
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageContainer} >
+                <ImageViewer placeholderImageSource={PlaceholderImage} />
+            </View>
+            <View style={styles.footerContainer} >
+                <Button theme='primary' label="Choose a photo" onPress={pickImageAsync} />
+                <Button label="Use this photo" />
+            </View>
+            <StatusBar style="auto" />
+        </View >
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  imageContainer: {
-    flex: 1,
-    paddingTop: 58,
-  },
-  footerContainer: {
-    flex: 1 / 3,
-    alignItems: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    imageContainer: {
+        flex: 1,
+        paddingTop: 58,
+    },
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
+    },
 });
--- react-native/StickerSmashApp/components/Button.js
+++ react-native/StickerSmashApp/components/Button.js
@@ -1,13 +1,13 @@
 import FontAwesome from '@expo/vector-icons/build/FontAwesome';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 
-export default function Button({ label, theme }) {
+export default function Button({ label, theme, onPress }) {
     if (theme === 'primary') {
         return (
             <View style={[styles.buttonContainer, { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 }]}>
                 <Pressable
                     style={[styles.button, { backgroundColor: '#fff' }]}
-                    onPress={() => alert('You pressed a button.')}
+                    onPress={onPress}
                 >
                     <FontAwesome
                         name='picture-o'
