751332f2fd685b3841b6510fa15105a41327212b
Li_Tian <Li_Tian@singaporeair.com.sg>
2024-03-15 11:02:38 +0800

    rn-stickersmash: craete ImageViewer component

--- react-native/StickerSmashApp/App.js
+++ react-native/StickerSmashApp/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import ImageViewer from './components/ImageViewer';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
@@ -7,7 +8,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer} >
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <StatusBar style="auto" />
     </View >
@@ -25,9 +26,4 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 58,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  }
 });
